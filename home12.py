4c95a6bd7f45823332cff20e19d2b6329f9f50d0
Sveta <margaritka2132@mail.ru>
2022-09-27 23:51:53 +0300

    added changes home12

---
+++ home12.py
@@ -0,0 +1,12 @@
+# Реализуйте алгоритм перемешивания списка.
+N = int (input('Bведите число > 0: '))
+import random
+list = []
+if N > 0:
+    for i in range(N):
+        list.append(i)
+    else:
+        print(list)
+        random_index = print(random.sample((list), N))
+else:
+    print("Ошибка ввода!")
