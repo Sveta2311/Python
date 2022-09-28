0984d59de6394fe06c6194e3b3d6c5afb76f32b8
Sveta <margaritka2132@mail.ru>
2022-09-28 15:15:37 +0300

    added changes home12

--- home12.py
+++ home12.py
@@ -1,12 +1,21 @@
-# Реализуйте алгоритм перемешивания списка.
 N = int (input('Bведите число > 0: '))
 import random
 list = []
+indexes = []
 if N > 0:
     for i in range(N):
         list.append(i)
     else:
-        print(list)
-        random_index = print(random.sample((list), N))
+        print(f"Исходный список: {list}")
+        newList = [None] * N
+        for i in range(N):
+            k = random.randint(0,N-1)
+            while k in indexes:
+                k = random.randint(0,N-1)
+            else:
+                indexes.append(k)
+                newList[k] = list[i]
+        else:
+            print(f"Перемешанный список: {newList}") 
 else:
     print("Ошибка ввода!")
